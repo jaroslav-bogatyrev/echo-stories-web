2dbc68cd51d14dba2bfbe1355b51c25b72ca710c
jaroslav-bogatyrev <jaroslav-bogatyrev@users.noreply.github.com>
2026-01-13 18:05:26 +0100

    fix: wrong email

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,5 +3,5 @@ import type { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   siteName: "Echo Stories",
   location: "Prague, Czech Republic",
-  email: "iuliia.mamaeva@echo-stories.com"
+  email: "iuliia.mamaeva@echo-stories.cz"
 };
